1c07738d0322cb09265dde7586e05d62a5f293e1
Richard Borges <richard.borges.cc@gmail.com>
2023-12-06 17:32:35 -0300

    chore: add style, navigate to user form

--- src/App.js
+++ src/App.js
@@ -3,16 +3,50 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import UserList from './views/UserList';
 import UserForm from './views/UserForm';
+import {Button, Icon} from '@rneui/base';
 
 const Stack = createStackNavigator();
 
 export default props => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="UserList">
-        <Stack.Screen name="UserList" component={UserList} />
-        <Stack.Screen name="UserForm" component={UserForm} />
+      <Stack.Navigator
+        initialRouteName="UserList"
+        screenOptions={screenOptions}>
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={({navigation}) => {
+            return {
+              title: 'Lista de Usuários',
+              headerRight: () => (
+                <Button
+                  onPress={() => navigation.navigate('UserForm')}
+                  type="clear"
+                  icon={<Icon name="add" size={25} color="white" />}
+                />
+              ),
+            };
+          }}
+        />
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{
+            title: 'Formulário de Usuários',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
+};
+
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#F4511E',
+  },
+  headerTintColor: '#FFFFFF',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
 };
