8fc8dd917e85654a6ade2ee242f617cfaea78340
Richard Borges <richard.borges.cc@gmail.com>
2023-12-07 09:20:52 -0300

    chore: users data

---
+++ src/data/users.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    id: 1,
+    name: 'João Silva',
+    email: 'josil@empmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png',
+  },
+];
