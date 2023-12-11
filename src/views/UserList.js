839eb6ab9afd106c960894648a6d0851ada94b6c
Richard Borges <richard.borges.cc@gmail.com>
2023-12-11 10:34:40 -0300

    chore: add user list action buttons

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,40 @@
 import React from 'react';
-import {View, Text, FlatList, ListItem} from 'react-native';
+import {View, Text, FlatList, ListItem, Alert} from 'react-native';
 import users from '../data/users';
+import {Button, Icon} from '@rneui/base';
 
 export default props => {
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+      {
+        text: 'Sim',
+        onPress() {
+          console.warn('delete' + user.id);
+        },
+      },
+      {
+        text: 'Não',
+      },
+    ]);
+  }
+
+  function getActions(user) {
+    return (
+      <>
+        <Button
+          onPress={() => props.navigation.navigate('UserForm', user)}
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+        />
+        <Button
+          onPress={() => confirmUserDeletion(user)}
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+        />
+      </>
+    );
+  }
+
   function getUserItem({item: user}) {
     return (
       <ListItem
@@ -11,7 +43,8 @@ export default props => {
         title={user.name}
         subtitle={user.email}
         bottomDivider
-        onPress={() => props.navigation.navigate('UserForm')}
+        rightElement={getActions(user)}
+        onPress={() => props.navigation.navigate('UserForm', user)}
       />
     );
   }
