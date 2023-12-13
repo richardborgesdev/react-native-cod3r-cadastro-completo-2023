dff846c9a2b638488be13bf2de0dd9f3e34fa814
Richard Borges <richard.borges.cc@gmail.com>
2023-12-13 10:12:42 -0300

    chore: form text input

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,8 +1,26 @@
 import React, {useState} from 'react';
 import {Text} from 'react';
+import {StyleSheet, TextInput, View} from 'react-native';
 
 export default ({route, navigation}) => {
   const [user, setUser] = useState(route.params ? route.params : {});
 
-  return <Text>{user.id}</Text>;
+  return (
+    <View>
+      <Text>Name</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={name => setUser({...user, name})}
+        placeholder="Informe o Nome"
+        value={user.name}
+      />
+    </View>
+  );
 };
+
+const style = StyleSheet.create({
+  input: {
+    height: 40,
+    borderColor: 'gray',
+  },
+});
