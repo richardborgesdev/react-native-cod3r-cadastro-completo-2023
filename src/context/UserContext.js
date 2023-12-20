f5370e402f4d52496ddc7e204d923108d53e3c99
Richard Borges <richard.borges.cc@gmail.com>
2023-12-20 11:59:28 -0300

    chore: users state and provider

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -3,4 +3,17 @@ import users from '../data/users';
 
 const UsersContext = createContext({});
 
+export const UsersProvider = props => {
+  return (
+    <UsersContext.Provider
+      value={{
+        state: {
+          users,
+        },
+      }}>
+      {props.children}
+    </UsersContext.Provider>
+  );
+};
+
 export default UsersContext;
