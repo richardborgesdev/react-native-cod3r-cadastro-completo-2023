4d9d8b637002a9631241d36a9e7d78def24cc50c
Richard Borges <richard.borges.cc@gmail.com>
2023-12-22 07:43:46 -0300

    chore: useRedeucer

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,15 +1,21 @@
-import React, {createContext} from 'react';
+import React, {createContext, useReducer} from 'react';
 import users from '../data/users';
 
+const initialState = {users};
 const UsersContext = createContext({});
 
 export const UsersProvider = props => {
+  function reducer(state, action) {
+    console.warn(state, action);
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <UsersContext.Provider
       value={{
-        state: {
-          users,
-        },
+        state,
+        dispatch,
       }}>
       {props.children}
     </UsersContext.Provider>
