b08ef005063f023f29e8c62f37464630a9c57c1e
Richard Borges <richard.borges.cc@gmail.com>
2024-01-10 14:30:03 -0300

    chore: dispatch create and update user

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {Text} from 'react';
 import {Button, StyleSheet, TextInput, View} from 'react-native';
+import UsersContext from '../context/UserContext';
 
 export default ({route, navigation}) => {
   const [user, setUser] = useState(route.params ? route.params : {});
+  const {dispatch} = useContext(UsersContext);
 
   return (
     <View style={style.form}>
@@ -31,6 +33,10 @@ export default ({route, navigation}) => {
       <Button
         title="Salvar"
         onPress={() => {
+          dispatch({
+            type: user.id ? 'updateUser' : 'createUser',
+            payload: user,
+          });
           navigation.goBack();
         }}
       />
